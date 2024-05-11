057e4fe080bb55f7aaca2336ca686c629912fe3e
AbidShazid3 <abidshazid3@gmail.com>
2024-05-12 04:37:44 +0600

    added my job delete action

--- src/Pages/MyJobs/MyJobs.jsx
+++ src/Pages/MyJobs/MyJobs.jsx
@@ -1,9 +1,102 @@
+import { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+
 
 
 const MyJobs = () => {
+    const { user } = useAuth();
+    const [myJobs, setMyJobs] = useState([]);
+    const axiosSecure = useAxiosSecure();
+
+    const url = `/jobs?email=${user?.email}`;
+
+    useEffect(() => {
+        axiosSecure.get(url)
+            .then(res => {
+                setMyJobs(res.data);
+            })
+    }, [axiosSecure, url])
+
+    const handleDelete = id => {
+        console.log(id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/jobs/${id}`, {
+                    method: 'DELETE',
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Has been deleted successfully.",
+                                icon: "success"
+                            });
+                            const remaining = myJobs.filter(newJob => newJob._id !== id);
+                            setMyJobs(remaining);
+                        }
+                    })
+            }
+        });
+    }
+
     return (
-        <div>
-            <h3>my job</h3>
+        <div className="mt-10">
+            <div className="text-center">
+                <h2 className="text-3xl font-bold text-slate-500 mb-3">My Added Jobs Collection</h2>
+                <p>View and manage the jobs you have added. Stay organized and keep track of your job postings in one place</p>
+            </div>
+            <div className="overflow-x-auto mt-5 bg-stone-300 p-4 rounded-xl">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Job title</th>
+                            <th>Post Date</th>
+                            <th>Job Deadline</th>
+                            <th>Job Salary</th>
+                            <th>Delete Job</th>
+                            <th>Update Job</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            myJobs.map((job, idx) => <tr key={job._id} className="hover">
+                                <th>{idx + 1}</th>
+                                <td>
+                                    <div>
+                                        <div>
+                                            <div className="font-bold">{job.jobTitle}</div>
+                                            <div className="text-sm opacity-50">{job.jobCategory}</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>{new Date(job.JobPostDate).toLocaleDateString()}</td>
+                                <td>{new Date(job.jobDeadline).toLocaleDateString()}</td>
+                                <td>{job.jobSalary}</td>
+                                <th>
+                                    <button onClick={() => handleDelete(job._id)} className="btn btn-xs btn-outline btn-error">Delete</button>
+                                </th>
+                                <th>
+                                    <button className="btn btn-xs btn-outline btn-accent">Update</button>
+                                </th>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
